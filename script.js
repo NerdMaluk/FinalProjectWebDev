2e067d00d340156fce93cbdd79fad8395ee5c4c4
Nerd <nerdmaluk@gmail.com>
2024-06-18 22:04:56 -0100

    footer fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+
 document.getElementById('contato-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
@@ -22,18 +22,21 @@ function mostrarAba(abaSelecionada){
     
     // 2 - Remover a classe "ativa" dos elemetos com a classe "aba"
     abas.forEach((aba)=>aba.classList.remove("ativa"));
+
     // 3 - Adicionar a classe "ativa" ao elemento que se deseja ativar 
     // O elemento que se deseja ativar é passado para a função através do atributo "abaSelecionada"
     document.getElementById(abaSelecionada).classList.add("ativa");
-    console.log("test"+ abas);
+    
+
     //4 - apanhar todos os elementos com a classe "conteudo"
     const conteudos = document.querySelectorAll(".conteudo");
-    console.log("conteudo"+ conteudos);
+    
     // 5 - remover a classe "ativa" dos elementos com a classe "conteudo"
     conteudos.forEach((conteudo) => conteudo.classList.remove("ativa"));
     
     // 6 - apanhar o id do conteúdo que se deseja apresentar, através da propriedade "data-aba" do elemento referenciado pelo id abaSelecionada
     const abaConteudo = document.getElementById(abaSelecionada).getAttribute("data-aba");
+    
     // 7 - adicona a classe "ativa" ao elemento que se deseja mostrar
     document.getElementById(abaConteudo).classList.add("ativa");
     
